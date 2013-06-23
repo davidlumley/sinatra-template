f2d70a90fb3052577f6ab6e294ec0640d1a137a2
David Lumley <david@davidlumley.com.au>
2013-06-23 23:28:18 +1000

    load asset initializer

--- config/boot.rb
+++ config/boot.rb
@@ -4,4 +4,5 @@ require 'rubygems'
 require 'bundler/setup'
 
 Bundler.require(:default, ENV['RACK_ENV'].to_sym)
+Dir.glob(File.expand_path('../initializers/**/*.rb', __FILE__), &method(:require))
 Dir.glob(File.expand_path('../../app/app**', __FILE__), &method(:require))
---
+++ config/initializers/assets.rb
@@ -0,0 +1,10 @@
+module Assets
+  module Javascripts
+    LOAD_PATH    = 'app/assets/javascripts'
+    COMPILE_PATH = 'public/assets'
+  end
+  module Stylesheets
+    LOAD_PATH    = 'app/assets/stylesheets'
+    COMPILE_PATH = 'public/assets'
+  end
+end
