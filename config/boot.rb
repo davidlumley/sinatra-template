05ee3929db6a369bc1322f8faa7257945059d9aa
David Lumley <david@davidlumley.com.au>
2013-08-29 12:19:32 +1000

    configure datamapper at boot

--- config/boot.rb
+++ config/boot.rb
@@ -6,3 +6,7 @@ require 'bundler/setup'
 Bundler.require(:default, ENV['RACK_ENV'].to_sym)
 Dir.glob(File.expand_path('../initializers/**/*.rb', __FILE__), &method(:require))
 Dir.glob(File.expand_path('../../app/app**', __FILE__), &method(:require))
+
+DataMapper.setup(:default, (ENV['DATABASE_URL'] || "sqlite3://#{Dir.pwd}/db/#{ENV['RACK_ENV']}.sqlite3"))
+Dir.glob(File.expand_path('../../app/models/**', __FILE__), &method(:require))
+DataMapper.auto_upgrade!
