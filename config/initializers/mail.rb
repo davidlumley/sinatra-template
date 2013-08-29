24fe142756cde23a02c8c5c70e2eee815ad5e79c
David Lumley <david@davidlumley.com.au>
2013-08-29 10:02:36 +1000

    fix sendgrid, add mocksmtp

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -1,17 +1,32 @@
 if ENV['RACK_ENV'] == 'production'
   Pony.options = {
-    :from                 => 'noreply@example.com',
-    :address              => 'smtp.sendgrid.net',
-    :port                 => '587',
-    :domain               => 'heroku.com',
-    :user_name            => ENV['SENDGRID_USERNAME'],
-    :password             => ENV['SENDGRID_PASSWORD'],
-    :authentication       => :plain,
-    :enable_starttls_auto => true
+    :from        => 'noreply@example.com',
+    :via         => :smtp,
+    :via_options => {
+      :address              => 'smtp.sendgrid.net',
+      :port                 => '587',
+      :domain               => 'heroku.com',
+      :user_name            => ENV['SENDGRID_USERNAME'],
+      :password             => ENV['SENDGRID_PASSWORD'],
+      :authentication       => :plain,
+      :enable_starttls_auto => true,
+    }
   }
 else
+  # SendMail
+  #Pony.options = {
+  #  :from        => 'noreply@example.com',
+  #  :via         => :sendmail,
+  #}
+
+  # MockSMTP
   Pony.options = {
+    :via         => :smtp,
     :from        => 'noreply@example.com',
-    :via         => :sendmail,
+    :via_options => {
+      :address        => 'localhost',
+      :port           => '1025',
+      :authentication => :login,
+    }
   }
 end
